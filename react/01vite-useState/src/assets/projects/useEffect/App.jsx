663105e30052ce71a9c26b8f88feb70a9792d303
Sween Khan <khansween@gmail.com>
2025-08-06 20:00:53 +0530

    learn about useEffect hook

--- react/01vite-useState/src/assets/projects/useEffect/App.jsx
+++ react/01vite-useState/src/assets/projects/useEffect/App.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+
 
 function App() {
+
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+
+    function fetchData() {
+
+      fetch("https://randomuser.me/api/")
+        .then((res) => {
+          return res.json()
+        })
+        .then((res) => {
+          console.log(res.results[0])
+          setData(res.results[0])
+        })
+    }
+
+    fetchData()
+
+  }, [])
+
+
+  // useEffect( () => {"logic" } , [])    //syntax
+
+
+
   return (
     <div>
-        <h2> use Efect hook </h2>
+      <h2> use Efect hook </h2> 
+
+      {
+        data.gender !== undefined
+        &&
+        <div>
+          <img src={data.picture.medium} />
+          <h2> {data.name.first}{data.name.last}</h2>
+          <p> {data.email}</p>
+        </div>
+
+      }
+
     </div>
   )
 }
--- react/01vite-useState/src/main.jsx
+++ react/01vite-useState/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css' 
-import App from './assets/projects/onChange/App'
+// import App from './assets/projects/onChange/App'
+import App from './assets/projects/useEffect/App'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ 
     <App />
-  </StrictMode>,
+ 
 )
